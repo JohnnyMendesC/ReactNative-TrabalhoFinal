dc5ec8f62d58d50f4b503827ea969285b5ee7763
JohnnyMendesC <johnnymendescarmo@gmail.com>
2024-11-20 14:07:08 -0300

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Splash } from './src/screens/Intro';
 import { Login } from './src/screens/Login';
 import { preventAutoHideAsync } from 'expo-splash-screen'
 
-preventAutoHideAsync(); //comentado para pular intro
+//preventAutoHideAsync(); //comentado para pular intro
 
 export default function App() {
   const [splashComplete, setSplashComplete] = useState(true);//true para testes sem a intro, padrao false
