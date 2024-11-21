b693efb22d526f0d18d5be1080ac342bccaa86f2
May4357 <marinamayumi1996@gmail.com>
2024-11-21 19:25:20 -0300

    atualização do navigator com o cadastro

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -45,6 +45,8 @@ const Navigation = () => {
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Perfil" component={Perfil} />
         <Tab.Screen name="Grupos" component={Grupos} />
+        <Tab.Screen name="Cadastro" component={Cadastro} />
+
       </Tab.Navigator>
     </NavigationContainer>
   );
