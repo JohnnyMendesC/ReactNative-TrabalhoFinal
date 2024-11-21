e39f36c19e910b6e28a5a0dbc0230bb331e3d315
May4357 <marinamayumi1996@gmail.com>
2024-11-21 01:38:54 -0300

    Iniciei o Perfil do usuário e a tela de Grupos

--- src/screens/Grupos/index.js
+++ src/screens/Grupos/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const Perfil = () => {
+const Grupos = () => {
   return (
     <View>
-      <Text>Perfil , definições e atividades.</Text>
+      <Text>Grupos</Text>
     </View>
   );
 };
 
-export default Perfil;
+export default Grupos;
