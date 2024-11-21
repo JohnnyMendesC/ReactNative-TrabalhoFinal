0c36da17f455d29d5ba06acc2036bce8d2603ae9
May4357 <marinamayumi1996@gmail.com>
2024-11-21 02:46:41 -0300

    correção navigator

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
-
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Splash } from './src/screens/Intro';
-import { Login } from './src/screens/Login';
 import Header from './src/components/Header';
 import { preventAutoHideAsync } from 'expo-splash-screen';
+import Navigation from './src/navigation/Navigation'; 
 
-preventAutoHideAsync(); 
+preventAutoHideAsync();
 
 export default function App() {
-  const [splashComplete, setSplashComplete] = useState(false); 
+  const [splashComplete, setSplashComplete] = useState(false);
 
   return (
     <>
-      <Header /> 
-      {splashComplete ? <Login /> : <Splash onComplete={setSplashComplete} />}
+      <Header />
+      {splashComplete ? <Navigation /> : <Splash onComplete={setSplashComplete} />}
     </>
   );
 }
